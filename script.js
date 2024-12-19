cde56bbf104a495f8e5ebcb70147813b4a087dec
gaimerI17 <gaimeri255@gmail.com>
2024-12-19 17:38:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -61,13 +61,13 @@ function loadPostContent(index) {
     let rankIcon;
     switch (topic.rank?.toLowerCase()) {
     case 'admin':
-        rankIcon = '<span class="glyph glyph-shield-security" onclick="alert(\'Tämä käyttäjä on admini, hänellä on kaikki mahdolliset oikeudet.\')"></span>';
+        rankIcon = '<span class="glyph glyph-shield-security" onclick="alert(\'Admin.\')"></span>';
         break;
     case 'banned':
-        rankIcon = '<span class="glyph glyph-shield-cross" onclick="alert(\'Tämä käyttäjä on bännätty; hänellä ei ole enää mitään oikeuksia.\')"></span>';
+        rankIcon = '<span class="glyph glyph-shield-cross" onclick="alert(\'Banned user\')"></span>';
         break;
     default: // Default to "Member" if rank is missing or not specified
-        rankIcon = '<span class="glyph glyph-shield-person" onclick="alert(\'Tämä käyttäjä on ihan tavallinen käyttäjä; ilman sen ihmeempiä.\')"></span>';
+        rankIcon = '<span class="glyph glyph-shield-person" onclick="alert(\'Member\')"></span>';
         break;
 }
 
